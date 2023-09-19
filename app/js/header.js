c4571dd0c971eaf4ced873e4079a5d4b18f82536
Daniil Popov <zikfaer12@gmail.com>
2023-09-19 08:25:48 +0700

    created adepth header

--- app/js/header.js
+++ app/js/header.js
@@ -6,13 +6,26 @@ function moveSocialMedia() {
   if (window.innerWidth < 870) {
     menu.insertBefore(socialMedia, menu.lastElementChild.nextSibling);
   } else {
-    headerWrapper.insertBefore(
-      socialMedia,
-      headerWrapper.lastElementChild.nextSibling
-    );
+    headerWrapper.insertBefore(socialMedia, headerWrapper.lastElementChild);
   }
 }
 
 moveSocialMedia();
 
 window.addEventListener("resize", moveSocialMedia);
+
+const menuBtn = document.querySelector(".menu__btn-drop-down");
+const menuMobile = document.getElementById("menu-list-id");
+const menuOpen = document.getElementById("drop-down-open-id");
+const menuClose = document.getElementById("drop-down-close-id");
+
+menuBtn.addEventListener("click", () => {
+  menuMobile.classList.toggle("menu__btn-drop-down_open");
+  if (menuMobile.classList.contains("menu__btn-drop-down_open")) {
+    menuOpen.style.display = "none";
+    menuClose.style.display = "block";
+  } else {
+    menuOpen.style.display = "block";
+    menuClose.style.display = "none";
+  }
+});
