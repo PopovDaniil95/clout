115d283b5ca33826f57b33914f72df69e0121d0f
Daniil Popov <zikfaer12@gmail.com>
2023-09-20 19:54:10 +0700

    fix

--- app/js/header.js
+++ app/js/header.js
@@ -1,6 +1,6 @@
 function moveSocialMedia() {
   let socialMedia = document.querySelector(".social-media");
-  let menu = document.querySelector(".menu ul");
+  let menu = document.querySelector(".menu");
   let headerWrapper = document.querySelector(".header__wrapper");
 
   if (window.innerWidth < 870) {
@@ -15,7 +15,7 @@ moveSocialMedia();
 window.addEventListener("resize", moveSocialMedia);
 
 const menuBtn = document.querySelector(".menu__btn-drop-down");
-const menuMobile = document.getElementById("menu-list-id");
+const menuMobile = document.getElementById("menu");
 const menuOpen = document.getElementById("drop-down-open-id");
 const menuClose = document.getElementById("drop-down-close-id");
 
