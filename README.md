a6eca4e7c02875ef2b45e3d86c4ad41a995a0915
Daniil Popov <zikfaer12@gmail.com>
2023-09-21 08:30:15 +0700

    fix adepth

--- README.md
+++ README.md
@@ -108,3 +108,26 @@
     top: -28px;
   }
 }
+
+
+
+скрипт для переноса соц.сетей в меню
+
+
+
+
+function moveSocialMedia() {
+  let socialMedia = document.querySelector(".social-media");
+  let menu = document.querySelector(".menu ul");
+  let headerWrapper = document.querySelector(".header__wrapper");
+
+  if (window.innerWidth < 870) {
+    menu.insertBefore(socialMedia, menu.lastElementChild.nextSibling);
+  } else {
+    headerWrapper.insertBefore(socialMedia, headerWrapper.lastElementChild);
+  }
+}
+
+moveSocialMedia();
+
+window.addEventListener("resize", moveSocialMedia);
--- app/js/header.js
+++ app/js/header.js
@@ -1,26 +1,12 @@
-function moveSocialMedia() {
-  let socialMedia = document.querySelector(".social-media");
-  let menu = document.querySelector(".menu");
-  let headerWrapper = document.querySelector(".header__wrapper");
-
-  if (window.innerWidth < 870) {
-    menu.insertBefore(socialMedia, menu.lastElementChild.nextSibling);
-  } else {
-    headerWrapper.insertBefore(socialMedia, headerWrapper.lastElementChild);
-  }
-}
-
-moveSocialMedia();
-
-window.addEventListener("resize", moveSocialMedia);
-
 const menuBtn = document.querySelector(".menu__btn-drop-down");
-const menuMobile = document.getElementById("menu");
+const menuMobile = document.getElementById("menu-list-id");
 const menuOpen = document.getElementById("drop-down-open-id");
 const menuClose = document.getElementById("drop-down-close-id");
+const socialMedia = document.querySelector(".social-media");
 
 menuBtn.addEventListener("click", () => {
   menuMobile.classList.toggle("menu__btn-drop-down_open");
+  socialMedia.classList.toggle("social-media_drop-down");
   if (menuMobile.classList.contains("menu__btn-drop-down_open")) {
     menuOpen.style.display = "none";
     menuClose.style.display = "block";
