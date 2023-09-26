d726a1eaae6f5e43558893f22c9031a4d4b397aa
Daniil Popov <zikfaer12@gmail.com>
2023-09-26 08:29:05 +0700

    make animation title

--- app/js/header.js
+++ app/js/header.js
@@ -3,15 +3,40 @@ const menuMobile = document.getElementById("menu-list-id");
 const menuOpen = document.getElementById("drop-down-open-id");
 const menuClose = document.getElementById("drop-down-close-id");
 const socialMedia = document.querySelector(".social-media");
+const body = document.querySelector("body");
 
 menuBtn.addEventListener("click", () => {
   menuMobile.classList.toggle("menu__btn-drop-down_open");
+  menuBtn.classList.toggle("menu__btn-drop-down_close");
   socialMedia.classList.toggle("social-media_drop-down");
   if (menuMobile.classList.contains("menu__btn-drop-down_open")) {
-    menuOpen.style.display = "none";
-    menuClose.style.display = "block";
+    body.style.overflowY = "hidden";
   } else {
-    menuOpen.style.display = "block";
-    menuClose.style.display = "none";
+    body.style.overflowY = "auto";
   }
 });
+
+const words = [
+  "КРЕАТИВ",
+  "ВИДЕО КОНТЕНТ",
+  "МОУШН ГРАФИКА",
+  "РЕКЛАМА",
+  "КЛИПЫ",
+  "ЭТО КЛАУТ",
+];
+let currentWordIndex = 0;
+
+// Функция для изменения слов в заголовке
+function changeWord() {
+  const title = document.getElementById("changingText");
+  title.textContent = words[currentWordIndex];
+  currentWordIndex = (currentWordIndex + 1) % words.length;
+
+  // Добавляем и убираем классы для анимации
+  title.classList.remove("off-screen");
+  void title.offsetWidth; // Принудительный перерасчет стилей
+  title.classList.add("off-screen");
+}
+
+// Установите интервал для смены слов
+setInterval(changeWord, 1000);
