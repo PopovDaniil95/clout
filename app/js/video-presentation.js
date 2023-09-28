403d17d98ede49c67913ee3ab9496f939ade0c3f
Daniil Popov <zikfaer12@gmail.com>
2023-09-28 21:36:14 +0700

    fix animation video

--- app/js/video-presentation.js
+++ app/js/video-presentation.js
@@ -1,31 +1,44 @@
+function getDistanceBetweenHeaderAndPromoTitle() {
+  var header = document.querySelector(".header");
+  var promoTitle = document.querySelector(".promo__title");
+
+  if (header && promoTitle) {
+    var headerRect = header.getBoundingClientRect();
+    var promoTitleRect = promoTitle.getBoundingClientRect();
+
+    var distance = promoTitleRect.top - headerRect.bottom;
+
+    console.log(
+      "Расстояние между header и promo__title:",
+      distance,
+      "пикселей"
+    );
+    return distance;
+  }
+}
+
 function videoCoordinates() {
   var promoTitle = document.querySelector(".promo__title");
   var videoPresentation = document.querySelector(".video-presentation");
+  var header = document.querySelector(".header");
 
   if (promoTitle) {
     var rect = promoTitle.getBoundingClientRect();
     var promoTitleTop = rect.top;
     var promoTitleHeight = promoTitle.offsetHeight;
 
-    var rect = videoPresentation.getBoundingClientRect();
-    var videoPresentationTop = rect.top;
+    var rectVideo = videoPresentation.getBoundingClientRect();
+    var videoPresentationTop = rectVideo.top;
+    let resultDistance = getDistanceBetweenHeaderAndPromoTitle();
 
-    var offsetTop =
-      videoPresentationTop - promoTitleTop + promoTitleHeight + 30;
-
-    var screenWidth =
-      window.innerWidth ||
-      document.documentElement.clientWidth ||
-      document.body.clientWidth;
-
-    if (screenWidth < 1150) {
-      var offsetTop = videoPresentationTop - promoTitleTop + promoTitleHeight;
-    }
-    if (screenWidth < 898) {
-      var offsetTop = videoPresentationTop - promoTitleTop + promoTitleHeight;
-    }
+    var offsetTop = videoPresentationTop - promoTitleTop + resultDistance;
 
     videoPresentation.style.top = -offsetTop + "px";
+
+    console.log(videoPresentationTop);
+    console.log(promoTitleTop);
+    console.log(promoTitleHeight);
+    console.log(offsetTop);
   }
 }
 videoCoordinates();
@@ -37,4 +50,3 @@ function refreshPage() {
   }
 }
 window.addEventListener("resize", refreshPage);
-
